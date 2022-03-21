fdee41eed773bb8dda0e95253d613b77c5ba068c
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-03-21 22:45:54 +0700

    add user registration

--- src/server.js
+++ src/server.js
@@ -5,8 +5,13 @@ const notes = require('./api/notes');
 const NoteService = require('./services/postgres/notesService');
 const NotesValidator = require('./validator/notes');
 
+const users = require('./api/users');
+const UsersService = require('./services/postgres/usersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesServices = new NoteService();
+  const usersServices = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -18,13 +23,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesServices,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesServices,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersServices,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
